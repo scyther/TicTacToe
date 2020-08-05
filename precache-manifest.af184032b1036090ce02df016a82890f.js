519347b0fcaa8b209c36e52c68c8baa6661b3329
Chirag <sharma.chirag3737@gmail.com>
2020-08-05 18:03:35 +0530

    Updates

---
+++ precache-manifest.af184032b1036090ce02df016a82890f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54a04bfdda697f0dc15d7df3ec14513e",
+    "url": "/TicTacToe/index.html"
+  },
+  {
+    "revision": "88422a3a9bbbefb52078",
+    "url": "/TicTacToe/static/css/2.e5b99fdb.chunk.css"
+  },
+  {
+    "revision": "9cfea5450103ab3a8bb2",
+    "url": "/TicTacToe/static/css/main.82a26bf7.chunk.css"
+  },
+  {
+    "revision": "88422a3a9bbbefb52078",
+    "url": "/TicTacToe/static/js/2.a462d4ff.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/TicTacToe/static/js/2.a462d4ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cfea5450103ab3a8bb2",
+    "url": "/TicTacToe/static/js/main.0f1bc611.chunk.js"
+  },
+  {
+    "revision": "4ee55b388125073784c8",
+    "url": "/TicTacToe/static/js/runtime-main.50844e1f.js"
+  }
+]);
